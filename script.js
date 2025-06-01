7673f436d99a0c7d34d47089feb63dadd1ed2825
Daisuke <komidakfox@gmail.com>
2025-06-01 09:40:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -63,3 +63,30 @@ const scrollHandler = () => {
 
 window.addEventListener('scroll', scrollHandler);
 window.addEventListener('load', scrollHandler);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const track = document.querySelector(".carousel-track");
+    const images = document.querySelectorAll(".carousel-track img");
+    const prevBtn = document.querySelector(".carousel-btn.prev");
+    const nextBtn = document.querySelector(".carousel-btn.next");
+    let Index = 0;
+
+    const updateSide = () => {
+        track.style.transform = `translateX(-${Index * 100}%)`;
+    };
+
+    nextBtn.addEventListener("click", () => {
+        Index = (Index + 1) % images.length;
+        updateSide();
+    });
+
+    prevBtn.addEventListener("click", () => {
+        Index = (Index - 1 + images.length) % images.length;
+        updateSide();
+    });
+
+    setInterval(() => {
+        Index = (Index + 1) % images.length;
+        updateSide();
+    }, 3000); // 自動スライド間隔(3秒)
+});
